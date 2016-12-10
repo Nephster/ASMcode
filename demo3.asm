02fe679efd2a5e3c0bf1d14a679a0d5ed8e28466
Luigi <luigicamastra@gmail.com>
2016-12-10 14:16:37 +0100

    second commit

--- demo3.asm
+++ demo3.asm
@@ -18,13 +18,13 @@ start:
 Main:
 
 pToAbsolut:
-  sub esp,4
+    sub esp,4
     pushad
     push ebp
     mov ebp,esp
     mov ebx,[ebp+40]
     push ebx
-    call func1
+    call func_absolut
     add esp,4
     mov [ebp+36],eax
     popad
@@ -40,7 +40,7 @@ pToRelative:
     mov ebp,esp
     mov ebx,[ebp+40]
     push ebx
-    call func2
+    call func_relative
     add esp,4
     mov [ebp+36],eax
     popad
@@ -48,97 +48,28 @@ pToRelative:
     add esp,4
     jmp DWORD PTR DS:[ebx] 
 
-func1:
+func_absolut:
 
     push ebp
     mov ebp,esp
     mov ebx,[ebp+8]
     sub ebx, 5
-    cmp ebx,0040E29Bh
-    jz jump4
-
-    cmp ebx,0040E2A9h
-    jz  jump5
-
-    cmp ebx,0040E2B6h
-    jz  jump4   
-    
-    cmp ebx,0040E2C0h
-    jz  jump4
-
-    cmp ebx,0040E2CEh
-    jz  jump5
-
-    cmp ebx,0040E2DBh
-    jz  jump4
-
-    cmp ebx,0040E2F1h
-    jz  jump4
-    
-    cmp ebx,0040E2F9h
-    jz  jump6
-    
+                        ; adding fucntion to sending to out blackbox
     mov esp,ebp
     pop ebp
     ret
 	
-jump4:
-    mov eax,004010B5h   ;printf
-    mov esp,ebp
-    pop ebp
-    ret 4
-
-jump5:
-    mov eax,00401000h   ;scanf
-    mov esp,ebp
-    pop ebp
-    ret 4
-
-jump6:
-    mov eax,0040E0B1h   ;getche
-    mov esp,ebp
-    pop ebp
-    ret 4
-
     
-
-func2:
+func_relative:
 
     push ebp
     mov ebp,esp
     mov ebx,[ebp+8]
     sub ebx, 6
-    cmp ebx,00401009h
-    jz  jump1
-
-    cmp ebx,0040101bh
-    jz  jump2
-    cmp ebx,0040102fh
-    jz  jump1       
-    cmp ebx,0040103fh
-    jz  jump1
-    cmp ebx,00401048h
-    jz  jump3
-    cmp ebx,0040104eh
-    jz  jump3
-	
-jump1:
-    mov eax,402000h
+                        ; adding fucntion to sending to out blackbox
     mov esp,ebp
     pop ebp
-    ret 4
-
-jump2:
-    mov eax,402004h
-    mov esp,ebp
-    pop ebp
-    ret 4
-
-jump3:
-    mov eax,402008h
-    mov esp,ebp
-    pop ebp
-    ret 4
+    ret 
 
 End Main
 
